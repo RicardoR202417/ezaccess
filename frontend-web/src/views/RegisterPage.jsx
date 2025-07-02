2606e3a43a3e680c0873c467c47ce037126c1b81
2024171007 <2024171007@uteq.edu.mx>
2025-07-02 09:31:26 -0600

    Elaboración de vistas login y registro con nueva estructura organizada de css en la app web.

---
+++ frontend-web/src/views/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+export default function RegisterPage() {
+  return (
+    <div className="admin-container">
+      <h2 className="form-title">Registro de Usuario</h2>
+      <RegisterForm />
+    </div>
+  );
+}
