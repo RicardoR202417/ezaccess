3ea72001474e6f9050c713b871b759c0bbb19baf
2024171007 <2024171007@uteq.edu.mx>
2025-07-14 10:37:03 -0600

    YAAAAA PORFAAAAA

--- frontend-web/src/views/LoginPage.jsx
+++ frontend-web/src/views/LoginPage.jsx
@@ -7,8 +7,9 @@ export default function LoginPage() {
     <div className="form-container d-flex justify-content-center align-items-center vh-100">
       <div className="form-box p-4 shadow-lg rounded bg-white" style={{ maxWidth: '480px', width: '150px' }}>
         <div className="text-center mb-4">
-          <img src={logo} alt="Logo del sistema" className="logo-login mb-4" />
-        </div>
+  <img src={logo} alt="Logo del sistema" className="logo-login" />
+</div>
+
         <h2 className="form-title text-center mb-3">Inicio de Sesión</h2>
         <LoginForm />
       </div>
