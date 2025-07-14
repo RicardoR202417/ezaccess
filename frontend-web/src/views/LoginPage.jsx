831a0d677e3c3f6f65057445de51c9bdff1789ae
2024171007 <2024171007@uteq.edu.mx>
2025-07-14 10:30:15 -0600

    ya bb, porfavor.

--- frontend-web/src/views/LoginPage.jsx
+++ frontend-web/src/views/LoginPage.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/logo.png';
 export default function LoginPage() {
   return (
     <div className="form-container d-flex justify-content-center align-items-center vh-100">
-      <div className="form-box p-4 shadow-lg rounded bg-white" style={{ maxWidth: '480px', width: '100%' }}>
+      <div className="form-box p-4 shadow-lg rounded bg-white" style={{ maxWidth: '480px', width: '150px' }}>
         <div className="text-center mb-4">
           <img src={logo} alt="Logo del sistema" className="logo-login mb-4" />
         </div>
