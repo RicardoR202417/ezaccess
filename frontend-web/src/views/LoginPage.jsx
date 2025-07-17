f8f6822a1f7bca188619080e82e707d9774c01f1
2024171007 <2024171007@uteq.edu.mx>
2025-07-16 22:50:31 -0600

    YA BB, PORFA

--- frontend-web/src/views/LoginPage.jsx
+++ frontend-web/src/views/LoginPage.jsx
@@ -4,10 +4,12 @@ import logo from '../assets/logo.png';
 
 export default function LoginPage() {
   return (
-    <div className="login-fullscreen">
-      <img src={logo} alt="Logo del sistema" className="logo-login" />
-      <h2 className="form-title">Inicio de Sesión</h2>
-      <LoginForm />
+    <div className="login-wrapper">
+      <div className="login-card">
+        <img src={logo} alt="Logo del sistema" className="logo-login" />
+        <h2 className="form-title">Inicio de Sesión</h2>
+        <LoginForm />
+      </div>
     </div>
   );
 }
