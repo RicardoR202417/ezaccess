21c639a5c12cf326eee3f72fc8b729ccd975ee66
2024171007 <2024171007@uteq.edu.mx>
2025-07-16 23:26:18 -0600

    miren como festejan el nuevo año chi chino en china

--- frontend-web/src/views/LoginPage.jsx
+++ frontend-web/src/views/LoginPage.jsx
@@ -4,10 +4,10 @@ import logo from '../assets/logo.png';
 
 export default function LoginPage() {
   return (
-    <div className="login-wrapper">
-      <div className="login-content">
-        <img src={logo} alt="Logo del sistema" className="logo-fixed" />
-        <h2 className="form-title">Inicio de Sesión</h2>
+    <div className="login-container">
+      <div className="login-box">
+        <img src={logo} alt="Logo del sistema" className="login-logo" />
+        <h2 className="login-title">Inicio de Sesión</h2>
         <LoginForm />
       </div>
     </div>
