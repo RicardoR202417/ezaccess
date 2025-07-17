c6c74490e906499a967f13aeac97df779f4cc41a
2024171007 <2024171007@uteq.edu.mx>
2025-07-17 09:39:33 -0600

    amor?

--- frontend-web/src/views/LoginPage.jsx
+++ frontend-web/src/views/LoginPage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
-import logo from '../assets/logo.jpg';
+import logo from '../assets/logo.png';
 
 export default function LoginPage() {
   return (
     <div className="form-container d-flex justify-content-center align-items-center vh-100">
       <div className="form-box p-4 shadow-lg rounded bg-white" style={{ maxWidth: '480px', width: '100%' }}>
         <div className="text-center mb-4">
-          <img src={logo} alt="Logo del sistema" className="logo-login" />
+          <img
+            src={logo}
+            alt="Logo del sistema"
+            className="logo-login"
+            style={{ width: '90px', height: '90px', objectFit: 'contain' }} // Tamaño reducido y proporcionado
+          />
         </div>
         <h2 className="form-title text-center mb-3">Inicio de Sesión</h2>
         <LoginForm />
