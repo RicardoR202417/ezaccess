96ff279050001aa866f4c9720531776819c7cfbc
2024171007 <2024171007@uteq.edu.mx>
2025-07-16 23:07:20 -0600

    BODEGUIX

--- frontend-web/src/views/LoginPage.jsx
+++ frontend-web/src/views/LoginPage.jsx
@@ -5,8 +5,8 @@ import logo from '../assets/logo.png';
 export default function LoginPage() {
   return (
     <div className="login-wrapper">
-      <div className="login-card">
-        <img src={logo} alt="Logo del sistema" className="logo-login" />
+      <div className="login-content">
+        <img src={logo} alt="Logo del sistema" className="logo-fixed" />
         <h2 className="form-title">Inicio de Sesión</h2>
         <LoginForm />
       </div>
