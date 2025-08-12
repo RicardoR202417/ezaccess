9a1d54a282d3635dbb7da7bc2b8967909002a7f9
Diego Reyes <racerblock2@outlook.com>
2025-08-12 06:06:51 -0600

    fix

--- backend/routes/nfcRoutes.js
+++ backend/routes/nfcRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const nfcController = require('../controllers/nfcController');
 
-router.post('/tagfija/:uid', nfcController.validarTagFijo);
+router.post('/tagfija/:uid', nfcController.validarNFC);
 
 module.exports = router;
