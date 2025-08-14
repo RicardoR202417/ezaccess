82a06a9abf76ee1bcd4017a589e91f5433cf5e96
Diego Reyes <racerblock2@outlook.com>
2025-08-14 11:18:10 -0600

    fix 1O

--- backend/controllers/cajonesController.js
+++ backend/controllers/cajonesController.js
@@ -30,7 +30,9 @@ exports.obtenerCajonesConEstado = async (req, res) => {
         id_caj: cajon.id_caj,
         numero_caj: cajon.numero_caj,
         ubicacion_caj: cajon.ubicacion_caj,
-        estado: asignacionActiva ? 'ocupado' : 'libre', // <- para filtro y color
+        estado: asignacionActiva
+  ? (asignacionActiva.estado_asig === 'activa' ? 'ocupado' : 'pendiente')
+  : 'libre', // <- para filtro y color
         estado_asig: asignacionActiva ? asignacionActiva.estado_asig : null, // <- para botón
         usuario_ocupante: asignacionActiva && asignacionActiva.Usuario
           ? asignacionActiva.Usuario.nombre_usu
